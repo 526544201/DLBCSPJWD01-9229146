04135bc8dec1c1230568587ebb2dbd4f356d9dbd
JReitmeister <526544201a@gmail.com>
2023-05-21 21:19:38 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DLBCSPJWD01-9229146',
+  webDir: 'build',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
