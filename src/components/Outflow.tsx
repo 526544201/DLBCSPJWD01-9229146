4586cbb5458f0607011bd9695de2aad05c911819
JReitmeister <526544201a@gmail.com>
2023-05-23 19:36:14 +0200

    Add Functionality Outflow

--- src/components/Outflow.tsx
+++ src/components/Outflow.tsx
@@ -5,7 +5,9 @@ import { IonButton, IonInput } from '@ionic/react';
 
 class Outflow extends Component {
     state = { // Holds data in the component
-        products: [] ,
+        products: [],
+        changedProducts: [],
+        requestId: ''
     }
 
     componentDidMount() { // Lifecycle method - When the component is mounted (on the screen)
@@ -17,6 +19,24 @@ class Outflow extends Component {
             .catch(error => { // Catch any errors
                 console.log(error); // DEBUG: Log the error to the console
             });
+    }
+
+    componentDidUpdate(prevProps: any, prevState: any) { // Lifecycle method - When the component is updated
+        if (prevState.changedProducts.length !== this.state.changedProducts.length) { // If the changedProducts array has changed
+            console.log('changedProducts changed'); // DEBUG
+            this.setState({requestId: this.createRequestId()}); // Create a new request id
+        }
+    }
+
+    createRequestId() {
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let result = '';
+        const charactersLength = characters.length;
+        for ( let i = 0; i < 15; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        console.log(result); // DEBUG
+        return result;
     }
 
     groupByCategory(products: any) {
@@ -32,11 +52,36 @@ class Outflow extends Component {
     }
 
     handleInputChange = (event: any, productId: any) => {
-        return;
+        const changedProducts: {productId: number, quantity: number}[] = [...this.state.changedProducts]; // Typescript doesn't like changedProducts.push({productId, quantity});
+        const quantity = event.target.value; // Get the value of the input
+        changedProducts.push({productId, quantity}); // Push the product id and quantity to the changedProducts array
+        this.setState({changedProducts}); // Set the state of the changedProducts array to the new array
+        console.log(this.state.changedProducts); // DEBUG
     }
 
     handleSubmit = (event: any) => {
-        return;
+        event.preventDefault();
+        const payload = this.createPayload();
+        axios.post(environment.apiUrl + '/bookStockchange.php', payload ) // Post the payload to the API via http request
+            .then(response => {
+                console.log(response); // DEBUG: Log the response to the console
+            })
+            .catch(error => { // Catch any errors
+                console.log(error); // DEBUG: Log the error to the console
+            });
+    }
+
+    createPayload() {
+        const changedProducts: {productId: number, quantity: number}[] = this.state.changedProducts;
+        const payload = {
+            timestamp: new Date().toISOString(),
+            type: 'Outflow',
+            requestId: this.state.requestId,
+            data: changedProducts
+        };
+
+        console.log(payload); // DEBUG
+        return payload;
     }
 
     render() { // Render the component
