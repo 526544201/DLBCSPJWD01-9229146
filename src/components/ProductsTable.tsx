703d41f36899e3db1ab030b62f995ac3d9128972
JReitmeister <526544201a@gmail.com>
2023-05-27 20:51:24 +0200

    Add ActionSheet
    If a user clicks on a product in the Products View, a Action Sheet pops up, allowing Edit or Delete of Product

--- src/components/ProductsTable.tsx
+++ src/components/ProductsTable.tsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import environment from '../environment';
-
+import { IonActionSheet, IonButton, IonContent } from '@ionic/react';
 
 import './Tables.css';
-import { IonContent } from '@ionic/react';
 
 class ProductsTable extends Component {
     state = { // Holds data in the component
-        products: [] ,
+        products: [],
+        selectedProduct: null as any
     }
 
     componentDidMount() { // Lifecycle method - When the component is mounted (on the screen)
@@ -22,9 +22,41 @@ class ProductsTable extends Component {
             });
     }
 
+    openActionSheet = (product: any) => {
+        this.setState({selectedProduct: product});
+    };
+
     render() { // Render the component
+        const { products, selectedProduct } = this.state;
         return ( // "Normal HTML" to be rendered
             <IonContent className="ion-padding">  { /* Only one element can be returned, so we wrap everything in a IonContent. This IonContent holds the table */ }
+                <IonActionSheet
+                    isOpen = {selectedProduct !== null} // If the user did not click on a product, do not show action sheet
+                    header={selectedProduct?.name ? selectedProduct.name : "Error - This should not happen!"} // If selectedProduct is not null, set header to nane of product. Otherwise display error
+                    onDidDismiss={() => this.setState({selectedProduct: null})} // When the action sheet is dismissed, set selectedProduct to null
+                    buttons={[
+                        {
+                            text: 'Edit',
+                            data: {
+                              action: 'edit',
+                            },
+                          },
+                        {
+                          text: 'Delete',
+                          role: 'destructive',
+                          data: {
+                            action: 'delete',
+                          },
+                        },
+                        {
+                          text: 'Cancel',
+                          role: 'cancel',
+                          data: {
+                            action: 'cancel',
+                          },
+                        },
+                      ]}
+                    ></IonActionSheet>
                 <table className="table">
                     <thead>
                         <tr>
@@ -37,11 +69,14 @@ class ProductsTable extends Component {
                     <tbody>
                         { /* Loop through the products array and create a row for each product */ }
                         {this.state.products.map((product: any) => (
-                            <tr key={product.id} className="clickable"> 
-                                <td>{product.name}</td>
-                                <td>{product.vendor_id}</td>
-                                <td>{product.stock}</td>
-                                <td>{product.minAmount}</td>
+                            <tr key={product.id} 
+                                className="clickable"
+                                onClick={() => this.openActionSheet(product)}
+                                > 
+                                    <td>{product.name}</td>
+                                    <td>{product.vendor_id}</td>
+                                    <td>{product.stock}</td>
+                                    <td>{product.minAmount}</td>
                             </tr>
                         ))}
                     </tbody>
